96351733373db427a27c86d25ea52f6c5f7d22c9
Николай Павлов <n.pavlov@i-sys.ru>
2023-07-28 17:45:54 +0400

    solved task

--- dz 2/dz2.js
+++ dz 2/dz2.js
@@ -1,4 +1,4 @@
-function clonDeepObj (dest, obj) {
+ function clonDeepObj (dest, obj) {
   const clonDeepObjKey = Array.isArray(obj)? [] : {}
   for(let key in obj)
   {
@@ -27,56 +27,84 @@ const obj2 = clonDeepObj({},obj );
 obj2.arr[0].end = 5
 obj2.arr[1].fix = false
 
-
 console.log(obj);
 console.log(obj2);
 
+  
 
 function arrayConvergence(arr1, arr2) {
-  const newArr = arr1.filter((item)=> arr2.includes(item))
-  return newArr
+  const valueByArr1 = arr1.reduce((acc, index) => {
+    if (acc[index]) {
+      acc[index] += 1;
+    } else {
+      acc[index] = 1;
+    }
+    return acc;
+  }, {});
+
+  const valueByArr2 = arr2.reduce((acc, index) => {
+    if (acc[index]) {
+      acc[index] += 1;
+    } else {
+      acc[index] = 1;
+    }
+    return acc;
+  }, {});
+
+  const crossbreeding = [];
+
+  Object.entries(valueByArr1).forEach(([num, count1]) => {
+    const count2 = valueByArr2[num] || 0;
+    const minCount = Math.min(count1, count2);
+    for (let i = 0; i < minCount; i++) {
+      crossbreeding.push(Number.parseInt(num, 10));
+    }
+  });
+
+  return crossbreeding;
 }
 
-const arr1 = [1, 1, 1, 2, 3, 4, 7, 7,7];
-const arr2 = [1,3,4,5,6,7,7,7,7] 
+const arr1 = [1, 1, 1, 2, 3, 4, 7, 7, 7];
+const arr2 = [1, 3, 4, 5, 6, 7];
 
 console.log(arrayConvergence(arr1, arr2));
 
 
-
-
 function getPropByPath(obj, path) {
-    let value = obj;
-    const keys = path.split(/\[|\]|\./).filter(key => key !== '');  
-  
-    keys.forEach((key) => {
-      if (value && typeof value === 'object') {
+  let value = obj;
+  const keys = path.split(/\[|\]|\./).filter(key => key !== '');  
 
-        if (Array.isArray(value) && /^\d+$/.test(key)) {
-          value = value[parseInt(key)];
-        } else if (key in value) {
-          value = value[key];
-        } else {
-          value = undefined;
-          console.log('Такого свойства нет в объекте');
-          return; 
-        }
+  for (let key of keys) {
+    if (value && typeof value === 'object') {
+      if (Array.isArray(value) && /^\d+$/.test(key)) {
+        value = value[Number.parseInt(key, 10)];
+      } else if (key in value) {
+        value = value[key];
       } else {
-        value = undefined;
         console.log('Такого свойства нет в объекте');
-        return; 
+        return undefined;
       }
-    }); 
-    return value;
+    } else {
+      console.log('Такого свойства нет в объекте');
+      return undefined;
+    }
   }
+  return value;
+}
 
 const newObj ={
   arr: [
     {
-    login: 'Test'
+      login: 'Test'
+    },
+    {
+      yourName: 'Name'
+    }
+  ],
+  person:{
+    personName: 'spider-man'
   }
-  ]
 }
-
-
 console.log(getPropByPath(newObj, 'arr[0].login'));
+console.log(getPropByPath(newObj, 'arr[1].yourName'))
+console.log(getPropByPath(newObj, 'person.personName'))
